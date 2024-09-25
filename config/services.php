8eb40db4c337cfb81aeef7b0221a631732fd18ca
Peyas  chandra das <peyas3500@gmail.com>
2024-09-25 15:32:58 +0600

    env example and config service update

--- config/services.php
+++ config/services.php
@@ -34,5 +34,9 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'nocaptcha' => [
+        'secret' => env('NOCAPTCHA_SECRET'),
+        'sitekey' => env('NOCAPTCHA_SITEKEY'),
+    ],
 
 ];
